32d915f895a1fdf180ab41b64aecf7820d612083
Kruthika <23eg110b31@anurag.edu.in>
2026-03-06 17:00:33 +0530

    Added dataset file

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# load data
+data = pd.read_csv("employee_dataset.csv")
+
+# cleaning
+data["age"] = pd.to_numeric(data["age"], errors="coerce")
+data["salary"] = pd.to_numeric(data["salary"], errors="coerce")
+
+data.dropna(inplace=True)
+
+X = data[["age"]]
+y = data["salary"]
+
+model = LinearRegression()
+model.fit(X, y)
+
+st.title("Salary Prediction App")
+
+age = st.number_input("Enter Age")
+
+if st.button("Predict Salary"):
+    result = model.predict([[age]])
+    st.success(f"Predicted Salary: {result[0]}")
